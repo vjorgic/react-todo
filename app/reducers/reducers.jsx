dcefc914e913292b8acd1d1163f5837a45abf541
Vlad <vjorgic@gmail.com>
2017-11-27 17:36:00 -0700

    Work on Toggle Todo

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -32,6 +32,17 @@ export var todosReducer = (state = [], action) => {
           completedAt: undefined
         }
       ];
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+        if(todo.id === action.id) {
+          var nextCompleted = !todo.completed;
+          return {
+            ...todo,
+            completed: nextCompleted,
+            completedAt: nextCompleted ? moment().unix() : undefined
+          };
+        }
+      });
     default:
       return state;
   };
